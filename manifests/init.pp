f986ff5967bd7478c54a31053329764a6bcc2a3a
Drew Blessing <drew.blessing@buckle.com>
2013-04-11 15:11:11 -0500

    Revert to eb225870045b3c7373f1e56f6580e700e0abba67

--- manifests/init.pp
+++ manifests/init.pp
@@ -46,7 +46,6 @@
 class logstash (
   $logstash_home                = $logstash::params::logstash_home,
   $logstash_etc                 = $logstash::params::logstash_etc,
-  $logstash_log                 = $logstash::params::logstash_log,
   $logstash_transport           = $logstash::params::logstash_transport,
   $logstash_jar_provider        = $logstash::params::logstash_jar_provider,
   $logstash_version             = $logstash::params::logstash_version,
@@ -86,7 +85,6 @@ class logstash (
 
   class { 'logstash::service':
     logstash_home               => $logstash_home,
-    logstash_log                => $logstash_log,
     logstash_etc                => $logstash_etc,
     logstash_transport          => $logstash_transport,
     logstash_verbose            => $logstash_verbose,
