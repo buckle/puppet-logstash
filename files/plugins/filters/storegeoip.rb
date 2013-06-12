6ba89cf61c9f33fb97d1bdf0ebdbbdbce6206a39
Drew Blessing <drew.blessing@buckle.com>
2013-06-12 15:00:35 -0500

    Geoip filter

---
+++ files/plugins/filters/storegeoip.rb
@@ -0,0 +1,85 @@
+require 'logstash/filters/base'
+require 'logstash/namespace'
+require 'json'
+require 'rest-client'
+require 'uri'
+
+class LogStash::Filters::StoreGeoIP < LogStash::Filters::Base
+  config_name "storegeoip"
+  plugin_status "experimental"
+  
+  # The field name that contains the store number
+  config :field, :validate => :string
+  
+  # The service bus URL for ip2store calls
+  config :service_bus_url, :validate => :string
+  
+  # The api key for service bus
+  config :api_key, :validate => :string
+  
+  # Specify into what field you want the store number placed.
+  config :target, :validate => :string, :default => 'geopoint'
+  
+  public
+  def register
+    @store_location = Hash.new({})
+    
+    @logger.debug "Registered storegeoip plugin", :config => @config
+  end
+  
+  public
+  def filter(event)
+    store_number = event[@field].to_i
+    full_url = URI.join(@service_bus_url, store_number.to_s + "/").to_s
+    event[@target] = {} if event[@target].nil?
+    
+    begin
+      if @store_location[store_number].empty? || expired(@store_location[store_number][:timestamp])
+        @logger.debug "Retrieving store location from Service Bus"
+          
+        response = RestClient.get full_url, :X_API_Key => @api_key
+        data = JSON.parse(response)
+        event[@target]['lat'] = to_geopoint(data['geoLatitude'])
+        event[@target]['lon'] = to_geopoint(data['geoLongitude'])
+        
+        cache_location store_number, to_geopoint(data['geoLatitude']), to_geopoint(data['geoLongitude'])
+         
+        else
+        @logger.debug "Retrieving store location from the cache"
+        
+        event[@target]['lat'] = @store_location[store_number][:lat]
+        event[@target]['lon'] = @store_location[store_number][:lon]
+      end
+      
+      #filter_matched(event)
+      
+      rescue SocketError => e
+      @logger.error "Could not find the Service Bus server. Please check DNS and verify the 'service_bus_url' configuration setting"
+        
+      rescue RestClient::BadGateway => e
+      @logger.error "Could not contact the Service Bus server.  Please check that Service Bus server is running, firewalls are not blocking the connection, and verify the 'service_bus_url' configuration setting"
+        
+      rescue Exception => e
+      @logger.error e
+        
+    end
+  end
+  
+  # Cache the store location with a key of store number. Set timestamp to now.
+  private
+  def cache_location(store_number, lat, lon)
+    @store_location[store_number] = { :lat => lat, :lon => lon, :timestamp => Time.now.to_i }
+  end
+  
+  # Given a timestamp from the cache, determine if it's been more than 24 hours
+  private
+  def expired(timestamp)
+    timestamp.to_i + 86400 < Time.now.to_i ? true : false
+  end
+  
+  # Convert lat/lon to geopoint lat/lon
+  private
+  def to_geopoint(point)
+    (point * 1E6).floor
+  end
+end
---
+++ manifests/filter/storegeoip.pp
@@ -0,0 +1,151 @@
+# == Define: logstash::filter::ip2store
+#
+#   The ip2store filter allows you to do general ip2storeations to fields that
+#   are not included in the normal mutate filter.  NOTE: The functionality
+#   provided by this plugin is likely to be merged into the 'mutate'
+#   filter in future versions.
+#
+#
+# === Parameters
+#
+# [*field*]
+#   Change the content of the field to the specified value if the content
+#   of another field is equal to the expected one.  Example:  filter {
+#   ip2store =&gt; {     condrewriteother =&gt; [           "field_name",
+#   "expected_value", "field_name_to_change", "value",
+#   "field_name2", "expected_value2, "field_name_to_change2", "value2",
+#   ....     ]   } }
+#   Value type is array
+#   Default value: None
+#   This variable is optional
+#
+# [*target*]
+#   Only handle events without any of these tags. Note this check is
+#   additional to type and tags.
+#   Value type is array
+#   Default value: []
+#   This variable is optional
+#
+# [*service_bus_url*]
+#   If this filter is successful, remove arbitrary tags from the event.
+#   Tags can be dynamic and include parts of the event using the %{field}
+#   syntax. Example:  filter {   myfilter {     remove_tag =&gt; [
+#   "foo_%{somefield}" ]   } }   If the event has field "somefield" ==
+#   "hello" this filter, on success, would remove the tag "foo_hello" if
+#   it is present
+#   Value type is array
+#   Default value: []
+#   This variable is optional
+#
+# [*api_key*]
+#   If this filter is successful, remove arbitrary tags from the event.
+#   Tags can be dynamic and include parts of the event using the %{field}
+#   syntax. Example:  filter {   myfilter {     remove_tag =&gt; [
+#   "foo_%{somefield}" ]   } }   If the event has field "somefield" ==
+#   "hello" this filter, on success, would remove the tag "foo_hello" if
+#   it is present
+#   Value type is array
+#   Default value: []
+#   This variable is optional
+#
+# [*tags*]
+#   Only handle events with all of these tags.  Note that if you specify a
+#   type, the event must also match that type. Optional.
+#   Value type is array
+#   Default value: []
+#   This variable is optional
+#
+# [*type*]
+#   The type to act on. If a type is given, then this filter will only act
+#   on messages with the same type. See any input plugin's "type"
+#   attribute for more. Optional.
+#   Value type is string
+#   Default value: ""
+#   This variable is optional
+#
+# [*order*]
+#   The order variable decides in which sequence the filters are loaded.
+#   Value type is number
+#   Default value: 10
+#   This variable is optional
+#
+#
+# === Examples
+#
+#
+#
+#
+# === Extra information
+#
+#  This define is created based on LogStash version 1.1.5
+#  Extra information about this filter can be found at:
+#  http://logstash.net/docs/1.1.5/filters/ip2store
+#
+#  Need help? http://logstash.net/docs/1.1.5/learn
+#
+# === Authors
+#
+# * Richard Pijnenburg <mailto:richard@ispavailability.com>
+#
+define logstash::filter::ip2store(
+  $field            = '',
+  $target           = '',
+  $service_bus_url  = '',
+  $api_key          = '',
+  $tags             = '',
+  $type             = '',
+  $order            = 10,
+) {
+
+  require logstash::params
+
+  #### Validate parameters
+  if $field {
+    validate_string($field)
+    $opt_field = "  field => \"${field}\"\n"
+  }
+
+  if $target {
+    validate_string($target)
+    $opt_target = "  target => \"${target}\"\n"
+  }
+
+  if $service_bus_url {
+    validate_string($service_bus_url)
+    $opt_service_bus_url = "  service_bus_url => \"${service_bus_url}\"\n"
+  }
+
+  if $api_key {
+    validate_string($api_key)
+    $opt_api_key = "  api_key => \"${api_key}\"\n"
+  }
+
+  if $tags {
+    validate_array($tags)
+    $arr_tags = join($tags, "', '")
+    $opt_tags = "  tags => ['${arr_tags}']\n"
+  }
+
+  if $order {
+    if ! is_numeric($order) {
+      fail("\"${order}\" is not a valid order parameter value")
+    }
+  }
+
+  if $type {
+    validate_string($type)
+    $opt_type = "  type => \"${type}\"\n"
+  }
+
+  #### Write config file
+
+  file { "${logstash::params::configdir}/filter_${order}_ip2store_${name}":
+    ensure  => present,
+    content => "filter {\n storegeoip {\n${opt_field}${opt_target}${opt_service_bus_url}${opt_api_key}${opt_tags}${opt_type} }\n}\n",
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    notify  => Class['logstash::service'],
+    require => Class['logstash::package', 'logstash::install']
+  }
+}
